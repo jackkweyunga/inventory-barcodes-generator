7886d2d2c3b4d0327b350a9d1c23e786d7e4c32e
jackkweyunga <jackkweyunga@gmail.com>
2021-09-09 04:36:43 +0300

    ...

--- src/main.py
+++ src/main.py
@@ -3,6 +3,10 @@ from barcode.writer import ImageWriter
 import base64
 from io import BytesIO
 from PIL import Image, ImageFont, ImageDraw
+import pathlib
+import os
+
+base_dir = pathlib.Path(__file__).parent.parent
 
 
 
@@ -21,15 +25,37 @@ class Bars:
     def __init__(self) -> None:
         pass
 
-    def generate(self, manufacturer_number, product_number, name=None, logo_path = None, logo_bytes=None, logo_b64 = None ):
+    def generate(self, manufacturer_number, product_number, name, logo_path = None, logo_bytes=None, logo_b64 = None ):
+
+        logo = ''
+        try:
+            if logo_b64:
+                logo = self.make_image_from_b64(logo_b64)
+            elif logo_bytes:
+                logo = self.make_image_from_bytes(logo_bytes)
+            elif logo_path:
+                logo = Image.open(logo_path)
+        except os.error as e:
+            print('\n\n')
+            print("error occured while trying toload the logo.")
+            print(e)
+            print('\n\n')
+
+            pass
+        
 
         number = f"{manufacturer_number}{product_number}"
+        code = self.make_upc(number) # the barcode
 
-        code = self.make_upc(number)
-
-        code_b64 = self.make_b64(code)
-
-        return code_b64
+        if type(logo) == Image:
+            output = self.add_details_to_image(code, number, name, logo)
+            code_b64 = self.make_b64(output) # the barcode in b64
+            return code_b64
+        else:
+            print('\n\n')
+            print("logo is not of type image")
+            print('\n\n')
+            return
 
 
     def make_upc(self, number):
@@ -49,8 +75,29 @@ class Bars:
         im = Image.open(BytesIO(base64.b64decode(b64)))
         return im
 
+    def make_image_from_bytes(self, bytes):
+        im = Image.open(BytesIO(bytes))
+        return im
 
+    def add_details_to_image(self, barcode_image, number, name, logo):
+        img1 = barcode_image
+        img2 = logo
 
+        output = Image.new('RGBA', (int(img1.size[0]*1.5), img1.size[1] + 100 ), (250, 250, 250, 0))
 
+        img2.thumbnail((int(img1.size[0]/2-80), img1.size[1]-80))
+        img1.thumbnail((img1.size[0], img1.size[1]-100))
 
+        output.paste(img2, (0,100))
+        output.paste(img1, (int(img1.size[0]/2),100))
+
+        draw = ImageDraw.Draw(output, 'RGBA')
+
+        text = name.upper()
+
+        font = ImageFont.truetype(os.path.join(base_dir, 'resources/fonts/Comfortaa/static/Comfortaa-Bold.ttf'), 80)
+        draw.text((0,0), text, (0,0,0), font=font, spacing=1.5, align='right')
+        draw.text((img2.size[0], output.size[1]-100), number, (0,0,0), font=font, spacing=30, align='right')
+
+        return output
 
