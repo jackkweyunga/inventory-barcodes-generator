b0941498f329299e4ca9fdbc31a37c7d58660d75
jackkweyunga <jackkweyunga@gmail.com>
2021-09-09 06:39:21 +0300

    ...

--- src/main.py
+++ src/main.py
@@ -9,8 +9,6 @@ import sys
 
 base_dir = pathlib.Path(__file__).parent.parent
 
-
-
 class Bars:
 
     '''
@@ -26,7 +24,7 @@ class Bars:
     def __init__(self) -> None:
         pass
 
-    def generate(self, manufacturer_number, product_number, name, logo_path = None, logo_bytes=None, logo_b64 = None ):
+    def generate(self, manufacturer_number, product_number, name, desc='', logo_path = None, logo_bytes=None, logo_b64 = None ):
 
         logo = ''
         try:
@@ -51,7 +49,7 @@ class Bars:
         # print(type(logo))
 
         if logo != '':
-            output = self.add_details_to_image(code, number, name, logo)
+            output = self.add_details_to_image(code, desc, name, logo)
             code_b64 = self.make_b64(output) # the barcode in b64
             return code_b64
         else:
@@ -90,21 +88,24 @@ class Bars:
         im = Image.open(BytesIO(bytes))
         return im
 
-    def add_details_to_image(self, barcode_image, number, name, logo):
+    def add_details_to_image(self, barcode_image, desc, name, logo):
         img1 = barcode_image
         img2 = logo
-        t_size = 30
-        t_height = 45
+        t_size = 24
+        t_height = 80
         padding = 10
         box_width = 3
 
-        img2.thumbnail((int(img2.size[0]), img1.size[1]))
+        img2.thumbnail((int(img1.size[0]*0.3), img1.size[1]))
         img1.thumbnail((img1.size[0], img1.size[1]))
 
-        output = Image.new('RGBA', (int(img1.size[0]+img2.size[0]+padding), img1.size[1]+t_height ), (250, 250, 250, 0))
+        img2 = img2.convert('L')
 
-        output.paste(img2, (padding,t_height-padding))
-        output.paste(img1, (int(img1.size[0]/2),t_height-padding))
+        output = Image.new('RGBA', (int(img1.size[0]+img2.size[0]+padding), img1.size[1]+t_height ), (250, 250, 250, 0))
+        W, H = output.size
+
+        output.paste(img2, (padding,int((t_height+padding))))
+        output.paste(img1, (int(img2.size[0]+padding),t_height+padding))
 
         draw = ImageDraw.Draw(output, 'RGBA')
 
@@ -113,29 +114,12 @@ class Bars:
         text = name.upper()
 
         font = ImageFont.truetype(os.path.join(base_dir, 'resources/fonts/Comfortaa/static/Comfortaa-Bold.ttf'), t_size)
-        draw.multiline_text((padding,padding), text, (0,0,0), font=font, spacing=1.5, align='right')
-        # draw.text((img2.size[0], output.size[1]-t_height), number, (0,0,0), font=font, spacing=30, align='right')
+        
+        w, h = draw.textsize(text, font=font)
+        draw.multiline_text(((padding+(W-w))/2,padding), text, (0,0,0), font=font, spacing=1.5, align='center')
+        
+        w, h = draw.textsize(desc, font=font)
+        draw.text((int((padding+(W-w))/2), t_height/2+padding), desc, (0,0,0), font=font, spacing=1.5, align='center')
 
         return output
 
-
-# tests
-
-b = Bars()
-
-logo = os.path.join(base_dir, 'resources/images/udsm-logo.jpg')
-
-x = b.generate(20000, 286770, 'Kinyerezi shop', logo_path=logo)
-
-x = b.make_image_from_b64(x)
-
-# print(x)
-
-x.show()
-
-
-# upc = barcode.get_barcode_class('upca')
-
-# code = upc('23332114343', ImageWriter())
-
-# print(code.render())
